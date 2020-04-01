a7fcd52d083cc4fda34dd2edc3df54ab1465ed73
FelipeEnne <felipeenne@gmail.com>
2020-04-01 13:37:38 -0300

    fix server.js and try do use style

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
 const http = require('http');
 
 const port = 8080;
-const ip = 'localhost';
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const responses = [];
-  responses['/'] = '<h1>Home</h1>';
-  responses['/inscreva-se'] = '<h1>Inscreva-se</h1>';
-  responses['/local'] = '<h1>Local</h1>';
-  responses['/contato'] = '<h1>Contato</h1>';
-  responses['/naoExiste'] = '<h1>URL sem resposta definida!</h1>';
+const handleRequest = (request, response) => {
+  response.writeHead(200, {
+    'Content-Type': 'text/html',
+  });
+  fs.readFile('./dist/index.html', null, (error, data) => {
+    if (error) {
+      response.writeHead(404);
+      response.write('file not found');
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
+};
 
-  res.end(responses[req.url] || responses['/naoExiste']);
-});
-
-server.listen(port, ip, () => {
-  console.log(`Server - http://${ip}:${port}`);
-  console.log('ctrl + c to leave');
-});
+http.createServer(handleRequest).listen(port);
